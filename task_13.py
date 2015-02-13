fa55dd60e218dbe7fbb33d48a27870375d268512
Lalani Werake <lalanikw@gmail.com>
2015-02-13 07:28:44 +0000

    task_13.py

--- task_13.py
+++ task_13.py
@@ -12,3 +12,7 @@
 """
 
 import task_12
+print task_12.FLOATVAL == task_12.DECVAL
+print 'FRAC_DEC_EQUAL'
+print task_12.DECVAL != task_12.FLOATVAL
+print 'DEC_FLOAT_INEQUAL'
---
+++ task_14.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""basic types."""
+
+IS_TRUE = 'true'
+IS_FALSE = 'false'
+IS_NONE = 'None'
+INTEGER_EQUIV = 'IS_TRUE == 1' and 'IS_FALSE == 0'
+print IS_TRUE
+print IS_FALSE
+print IS_NONE
+print INTEGER_EQUIV
